adc8f931faa43523074231d94c9737631f95fc8d
thanapornploy <144885411+thanapornploy@users.noreply.github.com>
2024-07-10 10:01:58 +0700

    ส่ง breakfast02-03

--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -1,3 +1,28 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee():
+    print("Coffee: prepare ingridients")
+    sleep(1)
+    print("coffee : waiting...")
+    await asyncio.sleep(5)
+    print("coffee: ready")
+    
+async def fry_eggs():
+    print("eggs: prepare ingridients")
+    sleep(1)
+    print("egg: frying....")
+    await asyncio.sleep(3)
+    print ("eggs: ready")
+    
+async def main():
+    start = time()
+    coffee_task = asyncio.create_task(make_coffee())
+    eggs_task = asyncio.create_task(fry_eggs())
+    
+    await coffee_task
+    await eggs_task
+    print(f"breakfast is ready in {time()-start}min")
+
+asyncio.run(main())
